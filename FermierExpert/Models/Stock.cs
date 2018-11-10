08c6d84a945689e00eaa3ed393d9c0a4001ba509
Balan Sabin Ionut <sabin_balan@yahoo.com>
2018-11-10 16:15:07 +0200

    add new models

---
+++ FermierExpert/Models/Stock.cs
@@ -0,0 +1,10 @@
+﻿namespace FermierExpert.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int ClientId { get; set; }
+        public int Ammount { get; set; }
+    }
+}
---
+++ FermierExpert/Models/Visit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FermierExpert.Models
+{
+    public class Visit
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string VisitReason { get; set; }
+        public int EmployeeId { get; set; }
+        public int ClientId { get; set; }
+    }
+}
