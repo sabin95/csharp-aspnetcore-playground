15e98a6f84107ab4a7fde3c6854915c443cf32cb
Balan Sabin Ionut <sabin_balan@yahoo.com>
2018-11-27 21:55:48 +0200

    Add Crop and Product Controller Tests

---
+++ FermierExpert.Tests/CropControllerTests.cs
@@ -0,0 +1,94 @@
+﻿using FermierExpert.Commands;
+using FermierExpert.Controllers;
+using FermierExpert.Data;
+using FermierExpert.Models;
+using ListaDubluInlantuita;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FermierExpert.Tests
+{
+    public class CropControllerTests
+    {
+        #region Add
+        [Fact]
+        public void Add_Should_Return_Bad_Request_On_Null_Commnad()
+        {
+            var controller = new CropsController(new Database { Crops = new ListaDubluInlantuita<Crop> { new Crop { Id = 1 } } });
+            var response = controller.AddCrop(null);
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+        [Fact]
+        public void Add_Should_Return_Bad_Request_On_Invalid_Id()
+        {
+            var controller = new CropsController(new Database { Crops = new ListaDubluInlantuita<Crop> { new Crop { Id = 1 } } });
+            var response = controller.AddCrop(new Commands.CropCommand
+            {
+                Id = 0
+            });
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+        [Fact]
+        public void Add_Should_Return_Bad_Request_On_Existing_Crop()
+        {
+            var controller = new CropsController(new Database { Crops = new ListaDubluInlantuita<Crop> { new Crop { Id = 1 } } });
+            var crop = new CropCommand { Id = 1 };
+            var response = controller.AddCrop(crop);
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+
+        [Fact]
+        public void Add_Should_Return_Ok()
+        {
+            var controller = new CropsController(new Database { Crops = new ListaDubluInlantuita<Crop> { new Crop { Id = 1 } } });
+            var crop = new CropCommand { Id = 2 };
+            var response = controller.AddCrop(crop);
+            Assert.IsType<OkResult>(response);
+        }
+        #endregion
+
+        #region Update
+        [Fact]
+        public void Update_Should_Return_Bad_Request_On_Null_Commnad()
+        {
+            var controller = new CropsController(new Database { Crops = new ListaDubluInlantuita<Crop> { new Crop { Id = 1 } } });
+            var response = controller.UpdateCrop(null);
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+        [Fact]
+        public void Update_Should_Return_Bad_Request_On_Invalid_Id()
+        {
+            var controller = new CropsController(new Database { Crops = new ListaDubluInlantuita<Crop> { new Crop { Id = 1 } } });
+            var response = controller.UpdateCrop(new Commands.CropCommand
+            {
+                Id = 0
+            });
+            Assert.IsType<BadRequestResult>(response);
+        }
+        [Fact]
+        public void Update_Should_Return_Bad_Request_On_NonExisting_Crop()
+        {
+            var controller = new CropsController(new Database { Crops = new ListaDubluInlantuita<Crop> { new Crop { Id = 1 } } });
+            var crop = new CropCommand { Id = 2 };
+            var response = controller.UpdateCrop(crop);
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+        [Fact]
+        public void Update_Should_Return_Ok()
+        {
+            var controller = new CropsController(new Database { Crops = new ListaDubluInlantuita<Crop> { new Crop { Id = 1 } } });
+            var crop = new CropCommand { Id = 1 };
+            var response = controller.UpdateCrop(crop);
+            Assert.IsType<OkResult>(response);
+        }
+        #endregion
+    }
+}
---
+++ FermierExpert.Tests/ProductControllerTests.cs
@@ -0,0 +1,114 @@
+﻿using FermierExpert.Commands;
+using FermierExpert.Controllers;
+using FermierExpert.Data;
+using FermierExpert.Models;
+using ListaDubluInlantuita;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FermierExpert.Tests
+{
+    public class ProductControllerTests
+    {
+        #region Add
+        [Fact]
+        public void Add_Should_Return_Bad_Request_On_Null_Commnad()
+        {
+            var controller = new ProductsController(new Database { Companies = new ListaDubluInlantuita<Company> { new Company { Id = 1 } }, Products = new ListaDubluInlantuita<Product> { new Product { Id = 1, CompanyId = 1 } } });
+            var response = controller.Add(null);
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+        [Fact]
+        public void Add_Should_Return_Bad_Request_On_Invalid_Id()
+        {
+            var controller = new ProductsController(new Database { Companies = new ListaDubluInlantuita<Company> { new Company { Id = 1 } }, Products = new ListaDubluInlantuita<Product> { new Product { Id = 1, CompanyId = 1 } } });
+            var response = controller.Add(new Commands.ProductCommand
+            {
+                Id = 0,
+                CompanyId = 1
+            });
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+        [Fact]
+        public void Add_Should_Return_Bad_Request_On_Existing_Product()
+        {
+            var controller = new ProductsController(new Database { Companies = new ListaDubluInlantuita<Company> { new Company { Id = 1 } }, Products = new ListaDubluInlantuita<Product> { new Product { Id = 1, CompanyId = 1 } } });
+            var product = new ProductCommand { Id = 1, CompanyId = 1 };
+            var response = controller.Add(product);
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+        [Fact]
+        public void Add_Should_Return_Bad_Request_On_Non_Existing_Company()
+        {
+            var controller = new ProductsController(new Database { Companies = new ListaDubluInlantuita<Company> { new Company { Id = 1 } }, Products = new ListaDubluInlantuita<Product> { new Product { Id = 1, CompanyId = 1 } } });
+            var product = new ProductCommand { Id = 1, CompanyId = 7 };
+            var response = controller.Add(product);
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+
+        [Fact]
+        public void Add_Should_Return_Ok()
+        {
+            var controller = new ProductsController(new Database { Companies = new ListaDubluInlantuita<Company> { new Company { Id = 1 } }, Products = new ListaDubluInlantuita<Product> { new Product { Id = 1, CompanyId = 1 } } });
+            var product = new ProductCommand { Id = 2, CompanyId = 1 };
+            var response = controller.Add(product);
+            Assert.IsType<OkResult>(response);
+        }
+        #endregion
+
+        #region Update
+        [Fact]
+        public void Update_Should_Return_Bad_Request_On_Null_Commnad()
+        {
+            var controller = new ProductsController(new Database { Companies = new ListaDubluInlantuita<Company> { new Company { Id = 1 } }, Products = new ListaDubluInlantuita<Product> { new Product { Id = 1, CompanyId = 1 } } });
+            var response = controller.Update(null);
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+        [Fact]
+        public void Update_Should_Return_Bad_Request_On_Invalid_Id()
+        {
+            var controller = new ProductsController(new Database { Companies = new ListaDubluInlantuita<Company> { new Company { Id = 1 } }, Products = new ListaDubluInlantuita<Product> { new Product { Id = 1, CompanyId = 1 } } });
+            var response = controller.Update(new Commands.ProductCommand
+            {
+                Id = 0,
+                CompanyId = 1
+            });
+            Assert.IsType<BadRequestResult>(response);
+        }
+        [Fact]
+        public void Update_Should_Return_Bad_Request_On_NonExisting_Product()
+        {
+            var controller = new ProductsController(new Database { Companies = new ListaDubluInlantuita<Company> { new Company { Id = 1 } }, Products = new ListaDubluInlantuita<Product> { new Product { Id = 1, CompanyId = 1 } } });
+            var product = new ProductCommand { Id = 7, CompanyId = 1 };
+            var response = controller.Update(product);
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+        [Fact]
+        public void Update_Should_Return_Bad_Request_On_NonExisting_Company()
+        {
+            var controller = new ProductsController(new Database { Companies = new ListaDubluInlantuita<Company> { new Company { Id = 1 } }, Products = new ListaDubluInlantuita<Product> { new Product { Id = 1, CompanyId = 1 } } });
+            var product = new ProductCommand { Id = 1, CompanyId = 6 };
+            var response = controller.Update(product);
+            Assert.IsType<BadRequestResult>(response);
+        }
+
+        [Fact]
+        public void Update_Should_Return_Ok()
+        {
+            var controller = new ProductsController(new Database { Companies = new ListaDubluInlantuita<Company> { new Company { Id = 1 } }, Products = new ListaDubluInlantuita<Product> { new Product { Id = 1, CompanyId = 1 } } });
+            var product = new ProductCommand { Id = 1, CompanyId = 1 };
+            var response = controller.Update(product);
+            Assert.IsType<OkResult>(response);
+        }
+        #endregion
+    }
+}
